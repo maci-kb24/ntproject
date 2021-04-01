eeb78849f1b73386eca3d4bb85bd743259287bbe
Marjan Stojanovski <macikb8@yahoo.com>
2021-04-01 16:50:21 +0200

    style changes

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+const navbar = document.getElementById("menu");
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
